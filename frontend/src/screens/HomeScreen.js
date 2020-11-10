dd833ab66d61c1d11f126f5135a2d74e93290a68
Eli-b-good <eli.i.henderson95@gmail.com>
2020-11-10 10:17:35 -0500

    removed orphaned video, set video to muted

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,11 @@
 import React from "react";
-import video from "../assets/Pexels Videos 2795400.mp4";
-import video2 from "../assets/production ID_3772428.mp4";
+
+import video from "../assets/production ID_3772428.mp4";
 import logo from "../assets/ExUiKHk - Imgur.png";
 const HomeScreen = () => {
   return (
     <div className="home-container">
-      <video src={video2} autoPlay loop mute="true" />
+      <video src={video} autoPlay loop muted="true" />
       <img src={logo} alt="PAM logo" />
     </div>
   );
